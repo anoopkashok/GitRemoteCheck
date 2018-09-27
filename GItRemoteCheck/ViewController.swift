614fa228313f78a182692323c1a174f4dbd0bed1
Anoop <anoop@meemmemory.com>
2018-09-27 17:36:39 +0530

    Initial Commit

---
+++ GItRemoteCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GItRemoteCheck
+//
+//  Created by Anoop on 27/09/18.
+//  Copyright © 2018 Anoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
